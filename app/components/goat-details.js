d1d5bae872b71285446255f1caecd2dfdb324840
Brett New and Stephanie Gurung <brett+stephanie@>
2016-08-24 13:50:11 -0700

    add functionality to allow user to add and remove goats from shopping cart; will not be able to add goat twice.

--- app/components/goat-details.js
+++ app/components/goat-details.js
@@ -2,11 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
-
+  inCart: Ember.computed('shoppingCart.items.[]', function(){
+    return this.get('shoppingCart').includes(this.get('goat'));
+  }),
   actions: {
     addToCart(item) {
       this.get('shoppingCart').add(item);
       this.sendAction('transitionCart');
+    },
+    removeFromCart(item) {
+      this.get('shoppingCart').remove(item);
     }
   }
 });
