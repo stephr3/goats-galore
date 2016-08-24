ce238700b3e251159801295d5d2f66aad3674b0f
Brett New and Stephanie Gurung <brett@>
2016-08-24 10:47:35 -0700

    redirect to shopping cart when a goat is added

---
+++ app/components/goat-details.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  shoppingCart: Ember.inject.service(),
+
+  actions: {
+    addToCart(item) {
+      this.get('shoppingCart').add(item);
+      this.sendAction('transitionCart');
+    }
+  }
+});
--- app/routes/goat.js
+++ app/routes/goat.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('goat', params.goat_id);
+  },
+  actions: {
+    transitionCart() {
+      this.transitionTo('cart');
+    }
   }
 });
