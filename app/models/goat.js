a23c972d70b0a06473a90b76de0f45170b1c6aaa
Brett New and Stephanie Gurung <brett@>
2016-08-24 10:14:24 -0700

    display goat listings and individual goat pages

---
+++ app/models/goat.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr(),
+  breed: DS.attr(),
+  price: DS.attr('number'),
+  image: DS.attr(),
+  description: DS.attr(),
+  category: DS.attr()
+});
