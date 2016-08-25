1dd49a6edd28124f7261998458e77e54faa5c39a
Brett New and Stephanie Gurung <stephanie@>
2016-08-25 13:24:59 -0700

    comment rating stars match actual rating

--- app/helpers/star-rating.js
+++ app/helpers/star-rating.js
@@ -1,17 +1,17 @@
 import Ember from 'ember';
 
 export function starRating(params) {
-  var comment = params[0];
-  if(comment === 1) {
+  var rating = params[0];
+  if(rating === 1) {
     return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span>');
-  } else if (comment === 2){
+  } else if (rating === 2){
     return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span>');
-  } else if (comment === 3){
+  } else if (rating === 3){
     return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span>');
-  } else if (comment === 4){
+  } else if (rating === 4){
     return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span>');
   } else {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span>');     
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span>');
   }
 }
 
