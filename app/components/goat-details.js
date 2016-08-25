c92b50072d39cd52a989663116ad326e97a00e80
Brett New and Stephanie Gurung <stephanie@>
2016-08-25 14:31:47 -0700

    display average rating as stars

--- app/components/goat-details.js
+++ app/components/goat-details.js
@@ -5,6 +5,16 @@ export default Ember.Component.extend({
   inCart: Ember.computed('shoppingCart.items.[]', function(){
     return this.get('shoppingCart').includes(this.get('goat'));
   }),
+  ratingAverage: Ember.computed('comments.@each.rating', function(){
+    var total = 0;
+    (this.get('comments')).forEach(function(comment){
+      console.log(comment.get('rating'));
+      total += comment.get('rating');
+      console.log(total);
+    });
+
+    return Math.round(total/this.get('comments').get('length'));
+  }),
   actions: {
     addToCart(item) {
       this.get('shoppingCart').add(item);
