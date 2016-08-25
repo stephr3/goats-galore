8c3dbfe1d9e3dbff81a980b30d47770e8f6f1cc3
Brett New and Stephanie Gurung <brett@>
2016-08-25 11:08:17 -0700

    display sold out sign when a goat has been added to the shopping cart

--- app/components/goat-tile.js
+++ app/components/goat-tile.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  shoppingCart: Ember.inject.service(),
+  inCart: Ember.computed('shoppingCart.items.[]', function(){
+    return this.get('shoppingCart').includes(this.get('goat'));
+  }),
 });
